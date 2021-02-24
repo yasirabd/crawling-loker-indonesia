31f4d1b070695a624a4c0b93e7b3a6d823ceb908
yasirabd <sir.yasirabd@gmail.com>
2021-02-24 21:20:49 +0700

    update class JobstreetItem

--- lokerid/lokerid/items.py
+++ lokerid/lokerid/items.py
@@ -18,7 +18,7 @@ class GlintsItem(scrapy.Item):
     skills_to_have = scrapy.Field()
 
 
-class JobstreetItem(scrapy.Item):
+class JobstreetItemOld(scrapy.Item):
     platform = scrapy.Field()
     job_position = scrapy.Field()
     company_name = scrapy.Field()
@@ -43,3 +43,37 @@ class JobstreetItem(scrapy.Item):
 
     # informasi perusahaan
     company_overview = scrapy.Field()
+
+class JobstreetItem(scrapy.Item):
+    # header
+    platform = scrapy.Field()
+    job_id = scrapy.Field()
+    posted_date = scrapy.Field()
+    closing_date = scrapy.Field()
+    job_position = scrapy.Field()
+    company_name = scrapy.Field()
+    salary = scrapy.Field()
+
+    # company detail
+    company_location = scrapy.Field()
+    avg_process_time = scrapy.Field()
+    telephone_number = scrapy.Field()
+    working_hours = scrapy.Field()
+    company_website = scrapy.Field()
+    company_size = scrapy.Field()
+    dress_code = scrapy.Field()
+    nearby_locations = scrapy.Field()
+    company_overview = scrapy.Field()
+
+    # job detail
+    job_description = scrapy.Field()
+    career_level = scrapy.Field()
+    years_of_experience = scrapy.Field()
+    qualification = scrapy.Field()
+    field_of_study = scrapy.Field()
+    industry = scrapy.Field()
+    skills = scrapy.Field()
+    employment_type = scrapy.Field()
+    languages = scrapy.Field()
+    job_function = scrapy.Field()
+    benefits = scrapy.Field()
